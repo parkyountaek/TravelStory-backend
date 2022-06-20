a7332ef8c8133684eda7a256d656c6bc6be818fa
unknown <yountaek11@gmail.com>
2022-06-21 00:35:40 +0900

    toy project start

---
+++ travel_story/src/main/java/com/travel_story/travel_story/TravelStoryApplication.java
@@ -0,0 +1,13 @@
+package com.travel_story.travel_story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelStoryApplication.class, args);
+	}
+
+}
---
+++ travel_story/src/test/java/com/travel_story/travel_story/TravelStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_story.travel_story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
